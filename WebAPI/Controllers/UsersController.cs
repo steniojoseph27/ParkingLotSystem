f5391c8db1c459c00a38e2e1c3a6bd0e2c10ec9e
Stenio Joseph <steniojoseph@hotmail.com>
2024-07-14 19:27:36 -0400

    Add: - Startup.cs to configure middleware and services. - Program.cs as entry point for the application, and UsersController.cs controller for managing users. Delete WheatherForCastController.cs

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigurationWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+}
