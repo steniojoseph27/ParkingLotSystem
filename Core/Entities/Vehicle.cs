96d694199709f027f2fce2a75d014e03d1bb0a5b
Stenio Joseph <steniojoseph@hotmail.com>
2024-07-27 17:44:13 -0400

    Add entities: ParkingSpace.cs, Reservation.cs, Vehicle.cs

---
+++ Core/Entities/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public int UserId { get; set; }
+        public string LicensePlate { get; set; }
+        public string Type { get; set; }
+    }
+}
