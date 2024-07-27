2a9ea15a4be5b9d882197e6c83ba1d7a801cad96
Stenio Joseph <steniojoseph@hotmail.com>
2024-07-27 04:57:25 -0400

    Feat: Implement IDbConnectionFactory interface and DbConnectionFactory class
    - IDbConnectionFactory provides a method to create database connections.
    - DbConnectionFactory retrieves the connection string from IConfiguration and returns a new SqlConnection instance.

---
+++ Infrastructure/Data/DbConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Infrastructure.Data
+{
+    public class DbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException(nameof(configuration), "Connection string 'DefaultConnection' is required.");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
