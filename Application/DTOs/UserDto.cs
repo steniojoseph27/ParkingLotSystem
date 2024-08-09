bcd939a7705c109c0bdb0e307ee74fe155b811e8
Stenio Joseph <steniojoseph@hotmail.com>
2024-08-09 14:46:00 -0400

    Feat: - Implement IEmailService contract for email related business logic. - EmailService class for email functionality. - Refactor UserDto, change the property Name to UserName.

--- Application/DTOs/UserDto.cs
+++ Application/DTOs/UserDto.cs
@@ -5,7 +5,7 @@ namespace Application.DTOs
     public class UserDto
     {
         public int UserId { get; set; }
-        public string Name { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }  
     }
---
+++ Infrastructure/Services/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Interfaces;
+
+namespace Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailAsync(string to, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
